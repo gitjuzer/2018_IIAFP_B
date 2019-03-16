492474fdbb36fa3fc936cf86feb47596da5f697f
AndorSebo <andorsebo@gmail.com>
2019-03-16 12:06:57 +0100

    Android - login felület elkészítve, még funkciók nélkül

--- Android app/OktatoApp/app/src/main/java/hu/afp/oktatoapp/MainActivity.java
+++ Android app/OktatoApp/app/src/main/java/hu/afp/oktatoapp/MainActivity.java
@@ -1,7 +1,10 @@
 package hu.afp.oktatoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View toolbar, student, teacher;
+        ImageView logo, studentLogo, teacherLogo;
+        TextView studentTV,teacherTV;
+
+        toolbar = findViewById(R.id.myToolbar);
+        student = findViewById(R.id.studentBtn);
+        teacher = findViewById(R.id.teacherBtn);
+
+        logo = toolbar.findViewById(R.id.logo);
+        studentLogo = student.findViewById(R.id.logo);
+        studentTV = student.findViewById(R.id.buttonText);
+        teacherLogo = teacher.findViewById(R.id.logo);
+        teacherTV = teacher.findViewById(R.id.buttonText);
+
+
+        logo.setImageDrawable(getDrawable(R.drawable.ic_puzzle));
+        studentLogo.setImageDrawable(getDrawable(R.drawable.ic_student));
+        teacherLogo.setImageDrawable(getDrawable(R.drawable.ic_teacher));
+        studentTV.setText(R.string.student);
+        teacherTV.setText(R.string.teacher);
+
     }
 }
