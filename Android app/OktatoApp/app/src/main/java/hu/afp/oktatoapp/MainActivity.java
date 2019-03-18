aa13e9fb5387b64c19c98b857e6a6c47bbc7d38f
AndorSebo <andorsebo@gmail.com>
2019-03-18 12:42:33 +0100

    - Login UI apróbb javítása - Logn UI alap metódusok megtervezve - gradle fájlba hozzáadva a volley és a guava library

--- Android app/OktatoApp/app/src/main/java/hu/afp/oktatoapp/MainActivity.java
+++ Android app/OktatoApp/app/src/main/java/hu/afp/oktatoapp/MainActivity.java
@@ -3,6 +3,8 @@ package hu.afp.oktatoapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,13 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText username, password;
         View toolbar, student, teacher;
         ImageView logo, studentLogo, teacherLogo;
-        TextView studentTV,teacherTV;
+        TextView studentTV, teacherTV;
+        Button login, register;
+
 
         toolbar = findViewById(R.id.myToolbar);
         student = findViewById(R.id.studentBtn);
         teacher = findViewById(R.id.teacherBtn);
+        login = findViewById(R.id.loginBtn);
+        register = findViewById(R.id.registerBtn);
+        username = findViewById(R.id.usernameET);
+        password = findViewById(R.id.passwordET);
 
         logo = toolbar.findViewById(R.id.logo);
         studentLogo = student.findViewById(R.id.logo);
@@ -33,5 +42,30 @@ public class MainActivity extends AppCompatActivity {
         studentTV.setText(R.string.student);
         teacherTV.setText(R.string.teacher);
 
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Student mode selected
+            }
+        });
+        teacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Teacher mode selected
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp1 = username.getText().toString();
+                String temp2 = password.getText().toString();
+                if (!("".contentEquals(temp1)) && !("".contentEquals(temp2)))
+                    sendLoginData(temp1, temp2);
+            }
+        });
+    }
+
+    private void sendLoginData(String username, String password) {
+        //JSON objektum küldése username, password mezőkkel
     }
 }
