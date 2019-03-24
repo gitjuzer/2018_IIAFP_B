9ed4fef23c115af58223a1f147aa3f1b6fae9f8e
Patrik Varga <patrikadamvarga@gmail.com>
2019-03-24 03:10:17 +0100

    Routingok irányítása a fő útvonalról

--- OktatoAppAPI/app.js
+++ OktatoAppAPI/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message:"Működik!"
-    });
+const userRoutes = require('./routes/user');
+const roleRoutes = require('./routes/role');
+
+app.use('/OktatoAppAPI/users', userRoutes);
+app.use('/OktatoAppAPI/roles', roleRoutes);
+
+app.use((req,res,next)=>{
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
 });
 
+app.use((error, req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+});
 module.exports = app;
