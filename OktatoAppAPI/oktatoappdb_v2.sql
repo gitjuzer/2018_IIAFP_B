b3b9bfc99a6e58bf9b35c0420452a374a40efd29
Patrik Varga <elyldor@gmail.com>
2019-04-01 20:26:12 +0200

    Adatbázis fájl frissítés, adatbázis tervek javítása

--- OktatoAppAPI/oktatoappdb_v2.sql
+++ OktatoAppAPI/oktatoappdb_v2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 31, 2019 at 01:40 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 01, 2019 at 06:13 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,9 +31,12 @@ USE `oktatoappdb`;
 -- Table structure for table `classroom`
 --
 
-CREATE TABLE `classroom` (
-  `id` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL
+DROP TABLE IF EXISTS `classroom`;
+CREATE TABLE IF NOT EXISTS `classroom` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -42,10 +45,14 @@ CREATE TABLE `classroom` (
 -- Table structure for table `classroom_to_game`
 --
 
-CREATE TABLE `classroom_to_game` (
+DROP TABLE IF EXISTS `classroom_to_game`;
+CREATE TABLE IF NOT EXISTS `classroom_to_game` (
   `is_active` tinyint(4) DEFAULT NULL,
   `game_id_pk` int(11) NOT NULL,
-  `classroom_id_pk` int(11) NOT NULL
+  `classroom_id_pk` int(11) NOT NULL,
+  PRIMARY KEY (`game_id_pk`,`classroom_id_pk`),
+  KEY `fk_Classroom_To_Game_Game1_idx` (`game_id_pk`),
+  KEY `fk_Classroom_To_Game_Classroom1_idx` (`classroom_id_pk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -54,9 +61,13 @@ CREATE TABLE `classroom_to_game` (
 -- Table structure for table `correct_answer`
 --
 
-CREATE TABLE `correct_answer` (
+DROP TABLE IF EXISTS `correct_answer`;
+CREATE TABLE IF NOT EXISTS `correct_answer` (
   `question_id` int(11) NOT NULL,
-  `correct_answer` varchar(250) NOT NULL
+  `correct_answer` varchar(250) NOT NULL,
+  PRIMARY KEY (`question_id`),
+  UNIQUE KEY `Question_Id_UNIQUE` (`question_id`),
+  KEY `fk_Correct_answer_Question1_idx` (`question_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -65,9 +76,13 @@ CREATE TABLE `correct_answer` (
 -- Table structure for table `difficulty`
 --
 
-CREATE TABLE `difficulty` (
+DROP TABLE IF EXISTS `difficulty`;
+CREATE TABLE IF NOT EXISTS `difficulty` (
   `difficulty_level` int(11) NOT NULL,
-  `diffculty_name` varchar(30) NOT NULL
+  `diffculty_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`difficulty_level`),
+  UNIQUE KEY `diffculty_name` (`diffculty_name`),
+  UNIQUE KEY `Diffculty_Name_UNIQUE` (`diffculty_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -76,10 +91,13 @@ CREATE TABLE `difficulty` (
 -- Table structure for table `game_mode`
 --
 
-CREATE TABLE `game_mode` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `game_mode`;
+CREATE TABLE IF NOT EXISTS `game_mode` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `description` varchar(250) DEFAULT NULL
+  `description` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -88,12 +106,17 @@ CREATE TABLE `game_mode` (
 -- Table structure for table `game_session`
 --
 
-CREATE TABLE `game_session` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `game_session`;
+CREATE TABLE IF NOT EXISTS `game_session` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `session_name` varchar(45) NOT NULL,
   `max_points` int(11) NOT NULL,
   `game_id` int(11) NOT NULL,
-  `difficulty_level` int(11) NOT NULL
+  `difficulty_level` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Session_name_UNIQUE` (`session_name`),
+  KEY `fk_Game_session_Game1_idx` (`game_id`),
+  KEY `fk_Game_session_Difficulty1_idx` (`difficulty_level`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -102,10 +125,13 @@ CREATE TABLE `game_session` (
 -- Table structure for table `permission`
 --
 
-CREATE TABLE `permission` (
-  `id` int(11) NOT NULL,
-  `permission_name` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE IF NOT EXISTS `permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permission_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `permission_name` (`permission_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -113,11 +139,14 @@ CREATE TABLE `permission` (
 -- Table structure for table `question`
 --
 
-CREATE TABLE `question` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE IF NOT EXISTS `question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `question` varchar(250) NOT NULL,
   `points` int(11) NOT NULL,
-  `game_session_id` int(11) NOT NULL
+  `game_session_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Question_Game_session1_idx` (`game_session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -126,11 +155,14 @@ CREATE TABLE `question` (
 -- Table structure for table `role`
 --
 
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `role_name` varchar(45) NOT NULL,
-  `role_description` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `role_description` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `role_name` (`role_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `role`
@@ -147,9 +179,13 @@ INSERT INTO `role` (`id`, `role_name`, `role_description`) VALUES
 -- Table structure for table `role_to_permission`
 --
 
-CREATE TABLE `role_to_permission` (
+DROP TABLE IF EXISTS `role_to_permission`;
+CREATE TABLE IF NOT EXISTS `role_to_permission` (
   `role_id_pk` int(11) NOT NULL,
-  `permission_id_pk` int(11) NOT NULL
+  `permission_id_pk` int(11) NOT NULL,
+  PRIMARY KEY (`role_id_pk`,`permission_id_pk`),
+  KEY `fk_role_to_permission_role1_idx` (`role_id_pk`),
+  KEY `fk_role_to_permission_permission1_idx` (`permission_id_pk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -158,12 +194,16 @@ CREATE TABLE `role_to_permission` (
 -- Table structure for table `statistic`
 --
 
-CREATE TABLE `statistic` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `statistic`;
+CREATE TABLE IF NOT EXISTS `statistic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `gained_points` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `user_id` int(11) NOT NULL,
-  `game_session_id` int(11) NOT NULL
+  `game_session_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Statistic_User1_idx` (`user_id`),
+  KEY `fk_Statistic_Game_session1_idx` (`game_session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -172,14 +212,17 @@ CREATE TABLE `statistic` (
 -- Table structure for table `token`
 --
 
-CREATE TABLE `token` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `token`;
+CREATE TABLE IF NOT EXISTS `token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `token` varchar(10000) NOT NULL,
   `created_at` datetime NOT NULL,
   `expires_at` datetime NOT NULL,
   `is_active` tinyint(4) DEFAULT NULL,
-  `user_Id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `user_Id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_token_user1_idx` (`user_Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -187,16 +230,20 @@ CREATE TABLE `token` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(16) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `password_hash` varchar(1000) NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
   `created_at` datetime NOT NULL,
-  `last_login` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `last_login` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -204,9 +251,13 @@ CREATE TABLE `user` (
 -- Table structure for table `user_to_classroom`
 --
 
-CREATE TABLE `user_to_classroom` (
+DROP TABLE IF EXISTS `user_to_classroom`;
+CREATE TABLE IF NOT EXISTS `user_to_classroom` (
   `user_id_pk` int(11) NOT NULL,
-  `classroom_id_pk` int(11) NOT NULL
+  `classroom_id_pk` int(11) NOT NULL,
+  PRIMARY KEY (`user_id_pk`,`classroom_id_pk`),
+  KEY `fk_User_To_Classroom_User1_idx` (`user_id_pk`),
+  KEY `fk_User_To_Classroom_Classroom1_idx` (`classroom_id_pk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -215,9 +266,13 @@ CREATE TABLE `user_to_classroom` (
 -- Table structure for table `user_to_role`
 --
 
-CREATE TABLE `user_to_role` (
+DROP TABLE IF EXISTS `user_to_role`;
+CREATE TABLE IF NOT EXISTS `user_to_role` (
   `user_id_pk` int(11) NOT NULL,
-  `role_id_pk` int(11) NOT NULL
+  `role_id_pk` int(11) NOT NULL,
+  PRIMARY KEY (`user_id_pk`,`role_id_pk`),
+  UNIQUE KEY `user_id_pk` (`user_id_pk`),
+  KEY `fk_user_to_role_role1` (`role_id_pk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -226,195 +281,14 @@ CREATE TABLE `user_to_role` (
 -- Table structure for table `wrong_answer`
 --
 
-CREATE TABLE `wrong_answer` (
+DROP TABLE IF EXISTS `wrong_answer`;
+CREATE TABLE IF NOT EXISTS `wrong_answer` (
   `id` varchar(45) NOT NULL,
   `wrong_answer` varchar(250) NOT NULL,
-  `question_id` int(11) NOT NULL
+  `question_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Wrong_answer_Question1_idx` (`question_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `classroom`
---
-ALTER TABLE `classroom`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
-
---
--- Indexes for table `classroom_to_game`
---
-ALTER TABLE `classroom_to_game`
-  ADD PRIMARY KEY (`game_id_pk`,`classroom_id_pk`),
-  ADD KEY `fk_Classroom_To_Game_Game1_idx` (`game_id_pk`),
-  ADD KEY `fk_Classroom_To_Game_Classroom1_idx` (`classroom_id_pk`);
-
---
--- Indexes for table `correct_answer`
---
-ALTER TABLE `correct_answer`
-  ADD PRIMARY KEY (`question_id`),
-  ADD UNIQUE KEY `Question_Id_UNIQUE` (`question_id`),
-  ADD KEY `fk_Correct_answer_Question1_idx` (`question_id`);
-
---
--- Indexes for table `difficulty`
---
-ALTER TABLE `difficulty`
-  ADD PRIMARY KEY (`difficulty_level`),
-  ADD UNIQUE KEY `diffculty_name` (`diffculty_name`),
-  ADD UNIQUE KEY `Diffculty_Name_UNIQUE` (`diffculty_name`);
-
---
--- Indexes for table `game_mode`
---
-ALTER TABLE `game_mode`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
-
---
--- Indexes for table `game_session`
---
-ALTER TABLE `game_session`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `Session_name_UNIQUE` (`session_name`),
-  ADD KEY `fk_Game_session_Game1_idx` (`game_id`),
-  ADD KEY `fk_Game_session_Difficulty1_idx` (`difficulty_level`);
-
---
--- Indexes for table `permission`
---
-ALTER TABLE `permission`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `permission_name` (`permission_name`);
-
---
--- Indexes for table `question`
---
-ALTER TABLE `question`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_Question_Game_session1_idx` (`game_session_id`);
-
---
--- Indexes for table `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `role_name` (`role_name`);
-
---
--- Indexes for table `role_to_permission`
---
-ALTER TABLE `role_to_permission`
-  ADD PRIMARY KEY (`role_id_pk`,`permission_id_pk`),
-  ADD KEY `fk_role_to_permission_role1_idx` (`role_id_pk`),
-  ADD KEY `fk_role_to_permission_permission1_idx` (`permission_id_pk`);
-
---
--- Indexes for table `statistic`
---
-ALTER TABLE `statistic`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_Statistic_User1_idx` (`user_id`),
-  ADD KEY `fk_Statistic_Game_session1_idx` (`game_session_id`);
-
---
--- Indexes for table `token`
---
-ALTER TABLE `token`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_token_user1_idx` (`user_Id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `user_to_classroom`
---
-ALTER TABLE `user_to_classroom`
-  ADD PRIMARY KEY (`user_id_pk`,`classroom_id_pk`),
-  ADD KEY `fk_User_To_Classroom_User1_idx` (`user_id_pk`),
-  ADD KEY `fk_User_To_Classroom_Classroom1_idx` (`classroom_id_pk`);
-
---
--- Indexes for table `user_to_role`
---
-ALTER TABLE `user_to_role`
-  ADD PRIMARY KEY (`user_id_pk`,`role_id_pk`),
-  ADD UNIQUE KEY `user_id_pk` (`user_id_pk`),
-  ADD KEY `fk_user_to_role_role1` (`role_id_pk`);
-
---
--- Indexes for table `wrong_answer`
---
-ALTER TABLE `wrong_answer`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_Wrong_answer_Question1_idx` (`question_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `classroom`
---
-ALTER TABLE `classroom`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `game_mode`
---
-ALTER TABLE `game_mode`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `game_session`
---
-ALTER TABLE `game_session`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `permission`
---
-ALTER TABLE `permission`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `question`
---
-ALTER TABLE `question`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `role`
---
-ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `statistic`
---
-ALTER TABLE `statistic`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `token`
---
-ALTER TABLE `token`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
