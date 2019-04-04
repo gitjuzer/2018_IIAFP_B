399efb36270a48a19209d49c36ab76ef7261186f
Unknown <barsimartin@gmail.com>
2019-04-04 21:44:57 +0200

    web - diák default layout

--- Web/Student/src/containers/Learning.js
+++ Web/Student/src/containers/Learning.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Learning.css'
 
 const Learning = props => {
   return (
-    <div>
-      Learning
+    <div className="learning-flex-container">
+      <div className="learning-side-bar">
+        <h1>Dropdown button</h1>
+      </div>
+      <div className="learning-main-content">
+        <h1>Témakörök</h1>
+      </div>
     </div>
   )
 }
