82b4bdaf915e4d998c8d0b2f3aa31498bf6d369b
Unknown <barsimartin@gmail.com>
2019-04-04 22:29:31 +0200

    web - diák custom dropdown component

--- Web/Student/src/containers/Learning.js
+++ Web/Student/src/containers/Learning.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import './Learning.css'
+import React from 'react';
+import './Learning.css';
+import Dropdown from '../components/dropdown';
 
 const Learning = props => {
   return (
     <div className="learning-flex-container">
       <div className="learning-side-bar">
-        <h1>Dropdown button</h1>
+        <Dropdown/>
       </div>
       <div className="learning-main-content">
         <h1>Témakörök</h1>
