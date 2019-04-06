b9cae1d9ead7a669362952333adc5796838683dd
Patrik Varga <patrikadamvarga@gmail.com>
2019-04-06 21:20:21 +0200

    check-auth refactor

--- OktatoAppAPI/middleware/check-auth.js
+++ OktatoAppAPI/middleware/check-auth.js
@@ -2,39 +2,19 @@ const jwt = require('jsonwebtoken')
 const Token = require('../models/token')
 const User = require('../models/user')
 
-exports.IsLoggedIn = (req,res,next)=>{
+IsAuthorized = (role1, role2, req, res, next)=>{
     try{
         const token = req.headers.authorization.split(' ')[1]
         const decoded = jwt.verify(token, process.env.JWT_KEY)
         req.user_data = decoded
         Token.getActiveTokensByUserId(decoded.user_id, (err,result)=>{
             if(result.length == 1){
-                next()
-            }
-            else{
-                return res.status(401).json({
-                    "status_code":"401",
-                    "description":"Jogosulatlan hozzáférés!"
-                })
-            }
-        })
-    } catch(error){
-        return res.status(401).json({
-            "status_code":"401",
-            "description":"Jogosulatlan hozzáférés!"
-        })
-    }
-}
-exports.IsAdmin = (req,res,next)=>{
-    try{
-        const token = req.headers.authorization.split(' ')[1]
-        const decoded = jwt.verify(token, process.env.JWT_KEY)
-        req.user_data = decoded
-        Token.getActiveTokensByUserId(decoded.user_id, (err,result)=>{
-            if(result.length == 1){
+                if(!role1 && !role2){
+                    return next()
+                }
                 User.getUserById(decoded.user_id, (err, result1)=>{
                     try{
-                        if(result1[0].role_name == 'ADMIN'){
+                        if(result1[0].role_name === (role1 || role2)){
                             next()
                         }
                         else{
@@ -63,79 +43,15 @@ exports.IsAdmin = (req,res,next)=>{
         })
     }
 }
-
-exports.IsTeacher = (req,res,next)=>{
-    try{
-        const token = req.headers.authorization.split(' ')[1]
-        const decoded = jwt.verify(token, process.env.JWT_KEY)
-        req.user_data = decoded
-        Token.getActiveTokensByUserId(decoded.user_id, (err,result)=>{
-            if(result.length == 1){
-                User.getUserById(decoded.user_id, (err, result1)=>{
-                    try{
-                        if(result1[0].role_name == 'TEACHER'){
-                            next()
-                        }
-                        else{
-                            throw new Error()
-                        }
-                    }catch(error){
-                        return res.status(401).json({
-                            "status_code":"401",
-                            "description":"Jogosulatlan hozzáférés!"
-                        })
-                    }
-                })
-            }
-            else{
-                return res.status(401).json({
-                    "status_code":"401",
-                    "description":"Jogosulatlan hozzáférés!"
-                })
-            }
-        })
-    } catch(error){
-        return res.status(401).json({
-            "status_code":"401",
-            "description":"Jogosulatlan hozzáférés!"
-        })
-    }
+exports.IsLoggedIn = (req,res,next)=>{
+    IsAuthorized(null,null, req, res,next)
 }
-
+exports.IsAdmin = (req,res,next)=>{
+    IsAuthorized('ADMIN',null,req,res,next)
+}
+exports.IsTeacher = (req,res,next)=>{
+    IsAuthorized('TEACHER',null, req,res,next)
+}
 exports.IsTeacherOrAdmin = (req,res,next)=>{
-    try{
-        const token = req.headers.authorization.split(' ')[1]
-        const decoded = jwt.verify(token, process.env.JWT_KEY)
-        req.user_data = decoded
-        Token.getActiveTokensByUserId(decoded.user_id, (err,result)=>{
-            if(result.length == 1){
-                User.getUserById(decoded.user_id, (err, result1)=>{
-                    try{
-                        if(result1[0].role_name == 'ADMIN' || result1[0].role_name == 'TEACHER'){
-                            next()
-                        }
-                        else{
-                            throw new Error()
-                        }
-                    }catch(error){
-                        return res.status(401).json({
-                            "status_code":"401",
-                            "description":"Jogosulatlan hozzáférés!"
-                        })
-                    }
-                })
-            }
-            else{
-                return res.status(401).json({
-                    "status_code":"401",
-                    "description":"Jogosulatlan hozzáférés!"
-                })
-            }
-        })
-    } catch(error){
-        return res.status(401).json({
-            "status_code":"401",
-            "description":"Jogosulatlan hozzáférés!"
-        })
-    }
+    IsAuthorized('ADMIN','TEACHER',req,res,next)
 }
