dcab4ad022b53c39dc0d9af9ff3e2952b3ead88d
Barsi Martin <barsimartin@gmail.com>
2019-04-14 22:44:13 -0700

    Web - Student - render game sessions

--- Web/Student/src/containers/Learning.js
+++ Web/Student/src/containers/Learning.js
@@ -8,33 +8,50 @@ import Gamesession from '../components/gamesession';
 class Learning extends React.Component {
     state = {
         gamemodes : {},
+        gamesessions : {},
     }
     componentDidMount(){
-        this.getGameModes();
+        this.getDataFromServer();
         
     }
-    getGameModes = () => {
+    getDataFromServer = () => {
         fetch("https://oktatoappapi.herokuapp.com/OktatoAppAPI/game-modes", {
             methor: "GET",
             headers: {
                 "Authorization": "Bearer " + this.props.token,
             }
         })
-            .then(response => response.json())
-            .then(responsejson => {
-                responseCodeTest(responsejson);
-                this.setState({gamemodes: responsejson.data});
-                console.log(this.state.gamemodes);
-            })
+        .then(response => response.json())
+        .then(responsejson => {
+            responseCodeTest(responsejson);
+            this.setState({gamemodes: responsejson.data});
+        })
+        fetch("https://oktatoappapi.herokuapp.com/OktatoAppAPI/game-sessions", {
+            methor: "GET",
+            headers: {
+                "Authorization": "Bearer " + this.props.token,
+            }
+        })
+        .then(response => response.json())
+        .then(responsejson => {
+            responseCodeTest(responsejson);
+            this.setState({gamesessions: responsejson.data});
+        })
     };
     renderGameModes = () => {
         return Array.from(this.state.gamemodes).map(gamemode => {
             return (
                 <Gamemode name={gamemode.name} description={gamemode.description} key={gamemode.id}>
+                    {this.renderGameSessions(gamemode.id)}
                 </Gamemode>
             )
             
         })
+    }
+    renderGameSessions = (game_id) => {
+        return Array.from(this.state.gamesessions).filter(gamesess => gamesess.game_id === game_id).map(gamesession => {
+            return <Gamesession key={gamesession.id} name={gamesession.session_name} />
+        });
     }
     render() {
         return (
