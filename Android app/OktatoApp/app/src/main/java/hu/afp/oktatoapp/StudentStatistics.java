bd5f6dac56f617add2ff37a56e78a98c263c6f19
Répás Kristóf <43805378+kaszasneni@users.noreply.github.com>
2019-04-28 11:56:21 +0200

    StudentStatistics bar chart rethink

--- Android app/OktatoApp/app/src/main/java/hu/afp/oktatoapp/StudentStatistics.java
+++ Android app/OktatoApp/app/src/main/java/hu/afp/oktatoapp/StudentStatistics.java
@@ -4,11 +4,17 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.ValueDependentColor;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentStatistics extends AppCompatActivity {
 
@@ -16,22 +22,59 @@ public class StudentStatistics extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_statistics);
+        drawChart();
+    }
 
-        GraphView graph = (GraphView) findViewById(R.id.barGraph);
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
-               new DataPoint(0, 5),
-               new DataPoint(1, 3),
-               new DataPoint(2, 2),
-               new DataPoint(3, 6)
-        });
-        graph.addSeries(series);
-        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
-            @Override
-            public int get(DataPoint data) {
-                return Color.rgb(0,255,0);
-            }
-        });
-        series.setDrawValuesOnTop(true);
-        series.setValuesOnTopColor(Color.RED);
+    private void drawChart() {
+        BarChart barChart = findViewById(R.id.barChart);
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        Description description = new Description();
+        description.setText("");
+        barChart.setDescription(description);
+        barChart.setMaxVisibleValueCount(50);
+        barChart.setPinchZoom(false);
+        barChart.setDrawGridBackground(false);
+
+        XAxis xl = barChart.getXAxis();
+        xl.setGranularity(1);
+        xl.setCenterAxisLabels(true);
+
+        YAxis leftAxis = barChart.getAxisLeft();
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setSpaceTop(35);
+        barChart.getAxisRight().setEnabled(false);
+
+        float barWidth = 0.8f;
+        int startMark = 1;
+        int endMark = 5;
+
+        List<BarEntry> yVals1 = new ArrayList<BarEntry>();
+
+        for (int i = startMark; i <= endMark; i++) {
+            yVals1.add(new BarEntry(i, i));
+        }
+
+        BarDataSet set1;
+
+        if (barChart.getData() != null && barChart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) barChart.getData().getDataSetByIndex(0);
+            set1.setValues(yVals1);
+            barChart.getData().notifyDataChanged();
+            barChart.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(yVals1, "Teszt Elek");
+            set1.setColor(Color.rgb(104, 241, 175));
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            barChart.setData(data);
+        }
+
+        barChart.getBarData().setBarWidth(barWidth);
+        //barChart.groupBars(startMark, groupSpace, barSpace);
+        barChart.invalidate();
     }
 }
