78ec7685a656e22935c8cd1c97a5968a52e349f2
palbalint <palbalint22@gmail.com>
2019-04-29 22:01:12 +0200

    Félig kész 404 page

--- Web/webapp/src/pages/404.js
+++ Web/webapp/src/pages/404.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 class Error extends React.Component {
-    state = {  }
+    state = { }
     render() { 
         return ( 
             <div className="pageNotFound">
-                <img src={ require('./img/sadStudent.png') } />
-                <h1 className="pnfH1">404 Page Not Found</h1>
+                <div className="backgr">
+                    <h1 className="pnfH1">404 Page Not Found</h1>
+                </div>
+                <div className="hahabox"></div>
                 <div>
-                    <p className="pnfP">Sorry, but the page you were trying to view does not exist.</p>
+                    <p className="pnfP">Sorry, you got lost in the galaxy, your only hope is the button below.</p>
                 </div>
                 <NavLink to="../"><button className="pnfButton">Back to home page</button></NavLink>
             </div>
          );
     }
 }
- 
+
 export default Error;
