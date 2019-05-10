e905c8407c77352e063ba5a5b2b2e519c45beaf6
BurusB <43416074+BurusB@users.noreply.github.com>
2019-05-10 14:39:52 +0200

    WebFront - QandA made it to work a bit better but sill broken

--- Web/webapp/src/loginedDashboard/games/QandA.js
+++ Web/webapp/src/loginedDashboard/games/QandA.js
@@ -13,17 +13,18 @@ class QandA extends React.Component {
         correct:0,
         inCorrect: 0,
 
-        wrongs : {},
-        corrects : {},
-        activeAnswer : "First"
+        wrongAnswers : {"null":null},
+        correctAnswer : null,
+        activeAnswer : null,
+        currentAnswer : null
     
     }
     componentDidMount() {
-       this.getQandA();
+       this.getQuestions();
     }
 
 
-    getQandA = () =>{
+    getQuestions = () =>{
         fetch("https://oktatoappapi.herokuapp.com/OktatoAppAPI/game-sessions/"+this.props.session_id+"/questions", {
             method: "GET",
             headers: {
@@ -35,7 +36,7 @@ class QandA extends React.Component {
         .then(responsejson => {
           if (responsejson.status_code === "201" || responsejson.status_code === "200")
             {
-            console.log(responsejson);  
+           // console.log(responsejson);  
             this.setState({questions : responsejson.data})
             this.setState({currentQuestion: this.state.questions[0]})
             console.table(this.state.currentQuestion)
@@ -44,13 +45,17 @@ class QandA extends React.Component {
            // this.setState({quest: tmp})
             //console.log(this.state.questions[0].question);
            // console.log(this.state.quest);
-           console.log(this.state.currentQuestion.question_id)
             }
           else
           {alert("Something went wrong with Authorization! Please try again later");}
+          this.getWrongAnswers();
+          this.getCorrectAnswer();
         });
+    }
+
+    getWrongAnswers = () => {
        // fetch("https://oktatoappapi.herokuapp.com/OktatoAppAPI/questions/"+this.state.currentQuestion.question_id+"/wrong-answers", { 
-        fetch("https://oktatoappapi.herokuapp.com/OktatoAppAPI/questions/"+21+"/wrong-answers", {
+        fetch("https://oktatoappapi.herokuapp.com/OktatoAppAPI/questions/"+this.state.currentQuestion.question_id+"/wrong-answers", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -61,8 +66,8 @@ class QandA extends React.Component {
         .then(responsejson => {
           if (responsejson.status_code === "201" || responsejson.status_code === "200")
             {
-            console.log(responsejson);  
-            this.setState({wrongs : responsejson.data})
+            //console.log(responsejson);  
+            this.setState({wrongAnswers : responsejson.data})
             }
           else
           {alert("mi?");}
@@ -70,27 +75,84 @@ class QandA extends React.Component {
 
     }
 
+    getCorrectAnswer = () => {
+        fetch("https://oktatoappapi.herokuapp.com/OktatoAppAPI/questions/"+this.state.currentQuestion.question_id+"/correct-answer", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "+ this.props.token
+            },
+        })
+        .then(response => response.json())
+        .then(responsejson => {
+          if (responsejson.status_code === "201" || responsejson.status_code === "200")
+            {
+           // console.log(responsejson);  
+            this.setState({correctAnswer : responsejson.data[0].correct_answer})
+          //  console.log(this.state.correctAnswer)
+            }
+          else
+          {alert("mi?");}
+        });
+    }
+
     changeSection = (newSection) => {
         this.setState({ activeAnswer: newSection });
+    }
+
+    checkIfAnySelected = () =>{
+        if(this.state.activeAnswer != null)
+        {
+            if(this.state.qIndex+1 <= this.state.questions.length){
+                this.checkAnswer();
+                this.setState({qIndex : this.state.qIndex+1})
+                alert("selected");
+                this.setState({currentQuestion : this.state.questions[this.state.qIndex]})
+                this.getCorrectAnswer();
+                this.getWrongAnswers();
+            }
+            else{
+                alert("tha game is offer");
+            }
+        }
+        else{
+            alert("Please select an answer first!");
+        }
+    }
+
+    setAnswer = (value) => {
+        this.setState({currentAnswer : value.target.value})
+        console.log(this.state.currentAnswer)
+    }
+
+    checkAnswer (){
+        if(this.state.currentAnswer === this.state.correctAnswer)
+        {
+            this.setState({correct :this.state.correct+1})
+            alert("zsamo")
+        }
+        else{
+            this.setState({inCorrect :this.state.inCorrect+1})
+        }
     }
     
     unselected = "answer";
     selected = "answer active";
-
+//this.state.wrongAnswers[0].wrong_answer
     renderCurrentQuestion = ()=>{
         return(
             <React.Fragment>
                 <div className="gameContent">
                     <div className="question">{this.state.currentQuestion.question}</div>
                     <div className="answers">
-                                    <button className={this.state.activeAnswer === "First" ? this.selected : this.unselected} onClick={() => this.changeSection("First")}>Fisrt</button>
-                                    <button className={this.state.activeAnswer === "Second" ? this.selected : this.unselected} onClick={() => this.changeSection("Second")}>SEC</button>    
+                                    <button className={this.state.activeAnswer === "First" ? this.selected : this.unselected} value={this.state.correctAnswer} onClick={(value) => {this.changeSection("First");  this.setAnswer(value) }}>{this.state.correctAnswer}</button>
+                                    <button className={this.state.activeAnswer === "Second" ? this.selected : this.unselected} value="anyád" onClick={(value) => {this.changeSection("Second");   this.setAnswer(value)}}>2</button>    
                                     <br></br>        
-                                    <button className={this.state.activeAnswer === "Third" ? this.selected : this.unselected} onClick={() => this.changeSection("Third")}>THIRD</button>
-                                    <button className={this.state.activeAnswer === "Fourth" ? this.selected : this.unselected} onClick={() => this.changeSection("Fourth")}>Fournegyedikhe</button>
+                                    <button className={this.state.activeAnswer === "Third" ? this.selected : this.unselected} value="meg neked is" onClick={(value) => {this.changeSection("Third");this.setAnswer(value)}}>3</button>
+                                    <button className={this.state.activeAnswer === "Fourth" ? this.selected : this.unselected} value="és neki is" onClick={(value) => {this.changeSection("Fourth");this.setAnswer(value)}}>4</button>
                     </div>
                 </div>
-                <div className="gameFooter"> why u put me down here <button onClick={()=>{this.passIntro()}}>Bye</button>
+                <div className="gameFooter"> why u put me down here <button onClick={()=>{this.checkIfAnySelected()}}>Bye</button>
                 </div>
             </React.Fragment>
         )}
