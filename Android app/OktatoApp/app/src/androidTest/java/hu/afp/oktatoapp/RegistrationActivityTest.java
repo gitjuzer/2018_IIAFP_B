415d15d86424d18a5f2c697ff091c06fc95ae97d
dezoox <levi1@windowslive.com>
2019-05-13 15:15:48 +0200

    Regisztrációs ablak teszt
    teszteli, hogy elindult-e a regisztrációs ablak, ha elindítja a felhasználó.

---
+++ Android app/OktatoApp/app/src/androidTest/java/hu/afp/oktatoapp/RegistrationActivityTest.java
@@ -0,0 +1,35 @@
+package hu.afp.oktatoapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegistrationActivityTest {
+
+    @Rule
+    public ActivityTestRule<RegistrationActivity> registrationActivityActivityTestRule = new ActivityTestRule<>(RegistrationActivity.class);
+
+    private RegistrationActivity registrationActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        registrationActivity = registrationActivityActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testMainActivityLaunch(){
+
+        View view = registrationActivity.findViewById(R.id.retryPassword_EditText);
+        assertNotNull(view);
+    }
+    @After
+    public void tearDown() throws Exception {
+        registrationActivity = null;
+    }
+}
