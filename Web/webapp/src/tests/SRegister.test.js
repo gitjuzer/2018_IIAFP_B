2c5b3925b480da21b6d5ce82c0121c2390a2f80c
BurusB <43416074+BurusB@users.noreply.github.com>
2019-05-17 00:22:53 +0200

    folder structure changes & unit test SLogin & SRegistration
    -changed runtest location
    -created folder for tests
    -removed basic js tests
    -cleard up some tests
    -added unit test for login form. Testing login with correct and incorrect data.
    -added unit test for registration form. Testing registration with correct and incorrect data.
    
    (cant seem to use fetch in SLogin.js, SRegister from tests. Maybe it should be done with some other method.)

---
+++ Web/webapp/src/tests/SRegister.test.js
@@ -0,0 +1,28 @@
+import Register from '../pages/SRegister';
+import { shallow, configure } from 'enzyme';
+import React  from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+test('Tests registration with inCorrect data', () => {
+    let instance =  shallow(<Register />).instance();
+    instance.register();
+    expect(instance.state.sucessful).toBeNull();
+})
+
+//it cant complete fetch, but it should be a working test.
+test('Tests registration with correct data', () => {
+    let instance =  shallow(<Register />).instance();
+    instance.setState({
+        "username": "fake",
+        "email": "youareFakenews@gmail.com",
+        "password": "howIamSupposedToRememberThis",
+        "first_name": "ItsAme",
+        "last_name": "Mario",
+        "account_type": "STUDENT"
+    });
+    instance.register();
+    console.log(instance.state.sucessful);
+    expect(instance.state.sucessful).not.toEqual(false);
+  })
