cc8f6a818652aef39ffaac447241a6d627183fab
Barsi Martin <barsimartin@gmail.com>
2019-05-15 22:45:56 -0700

    Web - Gamemode test, and enzyme module install

--- Web/webapp/src/login.test.js
+++ Web/webapp/src/login.test.js
@@ -1,20 +1,14 @@
 import Login from './pages/SLogin';
-import {shallow} from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import React, { Component } from 'react';
+import Header from './elements/header';
+import Learning from './loginedDashboard/containers/Learning';
+import Gamemode from './loginedDashboard/components/gamemode';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
 test('Check token recived on login', () => {
-  const login = shallow(<Login/>);
-  Login.login();
-  console.log("itt")
+  const wrapper = shallow(<Learning />);
+  expect(wrapper.contains(<Gamemode />)).toEqual(false);
 })
-
-
-const sum = require('./sum');
-
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
