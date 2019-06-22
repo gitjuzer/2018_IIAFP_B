5dc0939be3da1e11107213b64ee2a22b6a362b4a
tinzik <barsimartin@gmail.com>
2019-06-22 20:06:31 +0200

    Web - Unit test 1

--- Web/webapp/src/tests/gamemode.test.js
+++ Web/webapp/src/tests/gamemode.test.js
@@ -2,6 +2,7 @@ import { shallow, configure } from 'enzyme';
 import React from 'react';
 import Learning from '../loginedDashboard/containers/Learning';
 import Gamemode from '../loginedDashboard/components/gamemode';
+import Dropdown from '../loginedDashboard/components/dropdown';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
@@ -9,4 +10,9 @@ configure({ adapter: new Adapter() });
 test('Test gamemmode', () => {
   const wrapper = shallow(<Learning />);
   expect(wrapper.contains(<Gamemode />)).toEqual(false);
+})
+
+test('Test gamemmode2', () => {
+  const wrapper = shallow(<Learning />);
+  expect(wrapper.contains(<Dropdown />)).toEqual(false);
 })
